7e32ce1e5d3b68a3c273a446862ae91e1794d2b6
Nikhil Warke <nwarke@proton.me>
2024-01-08 17:24:38 +0530

    feat: Add tab navigation and routing
    Simple html tabs
    Add simple routing based by toggling hidden class

--- src/praveshika/core.cljs
+++ src/praveshika/core.cljs
@@ -3,6 +3,7 @@
   (:require [hiccups.runtime]))
 
 (def transactions (atom []))
+
 (defn load-transactions!
   "Load transactions from data store to app store"
   []
@@ -39,25 +40,14 @@
      (for [val tag]
        [:option {:value val} val])]))
 
-(defn- save-transaction []
-  (let [get-value (fn [element-id]
-                    (-> (.getElementById js/document element-id)
-                        .-value))
-        date (get-value "date")
-        payee (get-value "payee")
-        tag (get-value "tag")]
-    (swap! transactions conj
-           {:date date
-            :payee payee
-            :tag tag})
-    ;; Save to data store
-    (js/localStorage.setItem "transactions" @transactions)))
-
-
-(defn app []
+(defn- all-transactions-page []
   (html
-   [:header.mt-2 [:h1.text-center.text-2xl.font-semibold "Praveshika"]]
-   [:form#transaction.px-4.space-y-5
+   [:div#all-transactions-page.hidden
+    [:div.text-3xl] "Hi"]))
+
+(defn new-transaction-page []
+  [:div#new-transaction-page
+   [:form.px-4.space-y-5
     [:div.mt-2.grid.grid-cols-2.gap-x-6.gap-y-5
      [:div.col-span-full
       [:label.block.text-sm.font-medium.leading-6.text-gray-900 {:for "date"}
@@ -101,7 +91,49 @@
         "Comment"]
        [:input#comment {:type "text" :name "comment"}]]]]
     [:button#save-transaction.w-full.bg-blue-500.rounded-md.px-6.py-2.text-center.text-white
-     {:type "button"} "Save"]]))
+     {:type "button"} "Save"]]])
+
+(defn- save-transaction []
+  (let [get-value (fn [element-id]
+                    (-> (.getElementById js/document element-id)
+                        .-value))
+        date (get-value "date")
+        payee (get-value "payee")
+        tag (get-value "tag")]
+    (swap! transactions conj
+           {:date date
+            :payee payee
+            :tag tag})
+    ;; Save to data store
+    (js/localStorage.setItem "transactions" @transactions)))
+
+(defn route [route-id]
+  (let [all-element (.getElementById js/document "all-transactions-page")
+        new-element (.getElementById js/document "new-transaction-page")]
+    (condp = route-id
+      :new (do (.add (.-classList all-element) "hidden")
+               (.remove (.-classList new-element) "hidden"))
+      :all (do (.add (.-classList new-element) "hidden")
+               (.remove (.-classList all-element) "hidden")))))
+
+(defn- tab-nav-bar []
+  [:ul.flex.flex-wrap.text-sm.font-medium.text-center.text-gray-500.border-b.border-gray-200
+   [:li.me-2.grow
+    [:a#new-transaction-link.inline-block.p-4.text-blue-600.bg-gray-100.rounded-t-lg.active
+     {:aria-current "page"}
+     "New Transaction"]]
+   [:li.me-2.grow
+    [:a#all-transactions-link.inline-block.p-4.rounded-t-lg.hover:text-gray-600.hover:bg-gray-50
+     {:href "#"}
+     "All Transactions"]]])
+
+(defn app []
+  (html
+   [:header.mt-2 [:h1.text-center.text-2xl.font-semibold "Praveshika"]]
+   (tab-nav-bar)
+   [:div#shell
+    (new-transaction-page)
+    (all-transactions-page)]))
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn ^:dev/after-load main []
@@ -110,5 +142,8 @@
   (aset (.getElementById js/document "app") "innerHTML" (app))
   ;; Register Click Listeners
   (-> (.getElementById js/document "save-transaction")
-      (.addEventListener "click" save-transaction)))
-  
+      (.addEventListener "click" save-transaction))
+  (-> (.getElementById js/document "all-transactions-link")
+      (.addEventListener "click" #(route :all)))
+  (-> (.getElementById js/document "new-transaction-link")
+      (.addEventListener "click" #(route :new))))
