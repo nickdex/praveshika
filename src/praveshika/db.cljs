b2c1197e506d6af7fe7a99cae032568ee5ff9d14
Nikhil Warke <nwarke@proton.me>
2024-01-09 23:06:21 +0530

    fix: Default value of posting amount is 0

--- src/praveshika/db.cljs
+++ src/praveshika/db.cljs
@@ -3,3 +3,10 @@
 (def transactions (atom []))
 (defrecord Posting [account amount currency comment])
 (defrecord Transaction [date payee tag postings])
+
+#_{:clj-kondo/ignore [:redefined-var]}
+(defn ->Posting [account amount currency comment]
+  (Posting. account
+            (if (empty? amount) 0 amount)
+            currency
+            comment))
--- src/praveshika/new.cljs
+++ src/praveshika/new.cljs
@@ -63,7 +63,7 @@
       "Amount"]
      [:div.relative.mt-2.rounded-md.shadow-sm
       [:input#amount.posting-input.block.w-full.rounded-md.border-0.py-1.5.pr-20.text-gray-900.ring-1.ring-inset.ring-gray-300.placeholder:text-gray-400.focus:ring-2.focus:ring-inset.focus:ring-indigo-600.sm:text-sm.sm:leading-6
-       {:type "number"  :placeholder "0.00"}]
+       {:type "number" :placeholder "0.00"}]
       [:div.absolute.inset-y-0.right-0.flex.items-center
        [:label.sr-only {:for "currency"}
         "Currency"]
