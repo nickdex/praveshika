ef2da09772a56bc09f8f631f31d621494b809f44
Nikhil Warke <nwarke@proton.me>
2024-01-12 16:26:48 +0530

    chore: Update tailwind and postcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/index.html", "src/**/*.cljs"],
+  content: process.env.NODE_ENV === "production" ? ["./public/js/main.js"] : ["./public/index.html", "src/**/*.cljs"],
   theme: {
     extend: {},
   },
