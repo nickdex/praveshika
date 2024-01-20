b83c9321a148ccc0342df4eec3de7618e9265542
Nikhil Warke <nwarke@proton.me>
2024-01-20 12:42:34 +0530

    refactor: List editor component

--- src/main/praveshika/settings.cljs
+++ src/main/praveshika/settings.cljs
@@ -1,9 +1,10 @@
 (ns praveshika.settings
   (:require-macros [hiccups.core :refer [html]])
-  (:require [praveshika.db :as db]))
+  (:require [praveshika.db :as db]
+            [clojure.string :as str]))
 
 (defn- chip-li [item]
-  [:li.inline-block.p-2.text-center.text-sm.bg-green-300.m-2.rounded-full
+  [:li.p-2.text-center.text-sm
    [:span item]
    [:button.remove.float-right.rounded-full.ml-2.px-2.bg-red-200
     [:svg.w-4.h-5 {:xmlns "http://www.w3.org/2000/svg"
@@ -14,25 +15,48 @@
      [:path
       {:stroke-linecap "round" :stroke-linejoin "round" :d "M6 18 18 6M6 6l12 12"}]]]])
 
+(defn input
+  [{:keys [input-id label list default-value]}]
+  [:label.sr-only {:for input-id}
+   (str/capitalize label)]
+  [:div.w-full.inline-block.relative
+   [:input.w-full.rounded-md.border-0.py-1.pr-10.text-gray-900.ring-1.ring-inset.ring-gray-300.placeholder:text-gray-400.focus:ring-2.focus:ring-inset.focus:ring-indigo-600.sm:text-sm.sm:leading-6
+    {:name label
+     :id input-id
+     :placeholder  (str "Enter" " " label)
+     :value default-value
+     :list list}]
+   [:button.clear.absolute.z-10.right-0.top-1.pr-2.py-0.pl-2
+    [:svg.w-6.h-6 {:xmlns "http://www.w3.org/2000/svg"
+                   :fill "none"
+                   :viewBox "0 0 24 24"
+                   :stroke-width "1"
+                   :stroke "currentColor"}
+     [:path
+      {:stroke-linecap "round"
+       :stroke-linejoin "round"
+       :d "m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"}]]]])
+
+(defn- list-editor-component [{:keys [label items]}]
+  [:article
+   {:class label}
+   [:div.flex
+    (input {:input-id (str "new-" label)
+            :label label})
+    [:button.add.ml-2.bg-blue-500.px-2.text-white.rounded-lg "Add"]]
+   [:ul#payees.border.my-2.h-32.overflow-y-auto.divide-y
+    (for [item items]
+      (chip-li item))]])
+
 (defn home []
-  [:section.page.p-4.hidden
+  [:section.page.pt-4.px-4.hidden.gap-y-3.grid
    {:data-link "settings"}
-   [:article.payee
-    [:label.mr-2 "Payee"
-     [:input.ml-2 {:name "payee"
-                   :placeholder "Enter Payee"}]]
-    [:button.add.mx-2.bg-blue-500.p-2.text-white.rounded-lg "Add"]
-    [:ul#payees.border.my-2.h-32.overflow-y-auto
-     (for [item (db/get-all-payees)]
-       (chip-li item))]]
-   [:article.account
-    [:label.mr-2 "Account"
-     [:input.ml-2 {:name "account"
-                   :placeholder "Enter Account"}]]
-    [:button.add.mx-2.bg-blue-500.p-2.text-white.rounded-lg "Add"]
-    [:ul#accounts.border.my-2.h-32.overflow-y-auto
-     (for [item @db/accounts]
-       (chip-li item))]]])
+   (list-editor-component
+    {:label "payee"
+     :items (db/get-all-payees)})
+   (list-editor-component
+    {:label "account"
+     :items (db/get-all-accounts)})])
 
 (defn delete-payee!
   "Remove payee from app"
